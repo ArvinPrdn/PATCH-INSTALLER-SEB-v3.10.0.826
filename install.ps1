84d43dda2986c6e80323fb064a7dfbb541635219
ArvinPrdn <147386649+ArvinPrdn@users.noreply.github.com>
2026-01-26 10:35:36 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,18 +1,19 @@
-# Paksa TLS 1.2 (WAJIB untuk GitHub)
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-$Url = "https://github.com/ArvinPrdn/PATCH-INSTALLER-SEB-v3.10.0.826/releases/download/v3.10/patch-seb.exe"
+$Url = "https://github.com/ArvinPrdn/PATCH-INSTALLER-SEB-v3.10.0.826/raw/main/patch-seb.exe"
 $Out = "$env:TEMP\patch-seb.exe"
 
 Write-Host "Downloading Patch SEB..."
-Invoke-WebRequest -Uri $Url -OutFile $Out -UseBasicParsing
+try {
+    Invoke-WebRequest -Uri $Url -OutFile $Out -UseBasicParsing -ErrorAction Stop
+} catch {
+    Write-Host "❌ Download gagal. Jalankan manual dari GitHub."
+    exit
+}
 
 if (!(Test-Path $Out)) {
-    Write-Host "Download gagal. Installer tidak ditemukan."
-    exit 1
+    Write-Host "❌ File tidak ditemukan."
+    exit
 }
 
 Write-Host "Menjalankan installer..."
 Start-Process -FilePath $Out -Wait
-
 Write-Host "Selesai."
