b7e3424a5f0f7b11692e386627fc27a3a1f90839
ArvinPrdn <147386649+ArvinPrdn@users.noreply.github.com>
2026-01-26 10:44:16 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,12 +1,27 @@
-$Url = "PASTE_URL_EXE_DARI_RELEASE_DI_SINI"
+$Url = "https://github.com/ArvinPrdn/PATCH-INSTALLER-SEB-v3.10.0.826/releases/download/v3.10.0.826/patch-seb.1.exe"
 $Out = "$env:TEMP\patch-seb.exe"
 
 Write-Host "Downloading Patch SEB..."
 
-Invoke-WebRequest -Uri $Url -OutFile $Out -UseBasicParsing
+try {
+    Invoke-WebRequest `
+        -Uri $Url `
+        -OutFile $Out `
+        -UseBasicParsing `
+        -MaximumRedirection 10
+}
+catch {
+    Write-Error "❌ Gagal download file."
+    exit 1
+}
+
+if (!(Test-Path $Out)) {
+    Write-Error "❌ File tidak ditemukan setelah download."
+    exit 1
+}
 
 Write-Host "Menjalankan installer..."
 Unblock-File $Out
-Start-Process $Out -Wait
+Start-Process -FilePath $Out -Wait
 
-Write-Host "✅ Selesai."
+Write-Host "✅ Install selesai."
