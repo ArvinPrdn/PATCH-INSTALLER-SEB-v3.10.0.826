7d03ac17c25830df2b4b0e92b598685d92286591
ArvinPrdn <147386649+ArvinPrdn@users.noreply.github.com>
2026-01-26 10:40:44 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,19 +1,21 @@
-$Url = "https://github.com/ArvinPrdn/PATCH-INSTALLER-SEB-v3.10.0.826/raw/main/patch-seb.exe"
+$Url = "https://github.com/ArvinPrdn/PATCH-INSTALLER-SEB-v3.10.0.826/releases/download/v3.10.0.826/patch-seb.exe"
 $Out = "$env:TEMP\patch-seb.exe"
 
 Write-Host "Downloading Patch SEB..."
+
 try {
     Invoke-WebRequest -Uri $Url -OutFile $Out -UseBasicParsing -ErrorAction Stop
 } catch {
-    Write-Host "❌ Download gagal. Jalankan manual dari GitHub."
-    exit
+    Write-Host "❌ Gagal download file."
+    Write-Host "Cek koneksi atau URL release."
+    exit 1
 }
 
 if (!(Test-Path $Out)) {
-    Write-Host "❌ File tidak ditemukan."
-    exit
+    Write-Host "❌ File tidak ditemukan setelah download."
+    exit 1
 }
 
 Write-Host "Menjalankan installer..."
 Start-Process -FilePath $Out -Wait
-Write-Host "Selesai."
+Write-Host "✅ Installer selesai."
