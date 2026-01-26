4a3912604571441894701e156581ed97607fc7f0
ArvinPrdn <147386649+ArvinPrdn@users.noreply.github.com>
2026-01-26 10:32:33 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,1 +1,10 @@
-Write-Host "Installer siap"
+$Url = "https://github.com/ArvinPrdn/PATCH-INSTALLER-SEB-v3.10.0.826/releases/download/v3.10/patch-seb.exe"
+$Out = "$env:TEMP\patch-seb.exe"
+
+Write-Host "Downloading Patch SEB..."
+Invoke-WebRequest -Uri $Url -OutFile $Out
+
+Write-Host "Menjalankan installer..."
+Start-Process -FilePath $Out -Wait
+
+Write-Host "Selesai."
